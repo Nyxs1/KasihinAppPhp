374fb39c2b8830813411559b65b3b1241bf35ab5
Nyxs1 <endricopermana@gmail.com>
2025-07-08 21:23:28 +0700

    modif dikit

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 header("Location: controllers/auth/login.php");
+
+echo json_encode([
+    "message" => "Welcome to KasihinApp API",
+    "endpoints" => [
+        "Login" => "controllers/auth/login.php",
+        "Register" => "controllers/auth/register.php",
+        "Campaigns" => "controllers/campaign/get_all.php",
+        "Quests" => "controllers/quest/list.php",
+        "Donations" => "controllers/donation/history.php",
+    ]
+]);
 exit;
